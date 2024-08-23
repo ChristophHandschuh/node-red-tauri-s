801ab299f01a13e099a75030d9fbc3faf3498606
Christoph Handschuh <chrisi.handschuh@gmail.com>
2024-08-23 11:45:23 +0200

    release

--- binary_output.js
+++ binary_output.js
@@ -1,5 +1,21 @@
 module.exports = function(RED) {
     "use strict";
+    const pinMap = {0: [0x12, 0b10000000],
+                    1: [0x12, 0b01000000],
+                    2: [0x12, 0b00100000],
+                    3: [0x12, 0b00010000],
+                    4: [0x12, 0b00001000],
+                    5: [0x12, 0b00000100],
+                    6: [0x13, 0b00001000],
+                    7: [0x13, 0b00000100],
+                    8: [0x13, 0b00000010],
+                    9: [0x13, 0b00000001],
+                    10: [0x12, 0b00000001],
+                    11: [0x12, 0b00000010]}
+
+    var currentPinsA = 0b00000000;
+    var currentPinsB = 0b00000000;
+
     try {
         var i2c = require("i2c-bus");   
     } catch (error) {
@@ -7,57 +23,189 @@ module.exports = function(RED) {
         i2c = null;
     }
 
-    function validateI2C(node) {
-        if (!i2c) {
-            node.log("Couldn't load i2c-bus, try npm i i2c-bus");
-            node.status({fill:"gray",shape:"dot",text:"unsupported"});
-            if (i2c_error) console.error(i2c_error);
-            return false;
-        }
-        return true;
-    }
-
-    function validateMSG(str) {
-        try{
-            const obj = JSON.parse(str);
-            if(obj.hasOwnProperty("pin") && obj.hasOwnProperty("state")){
-                return true;
-            }
-        }catch (error) {
-            return false;
-        }
-        return false;
-    }
-
     function binary_output(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        var activeCMD = false;
+        var busAddress = 0x20 + parseInt(config.modnum, 16);
+        node.log(busAddress);
 
-        if (!validateI2C(node)) return;
+        /*** GLOBAL FUNCTIONS ***/
+        function validateMSG(obj) {
+            try{
+                if(obj.hasOwnProperty("signalname") && obj.hasOwnProperty("state") && (obj["state"] === "HIGH" || obj["state"] === "LOW")){
+                    return true;
+                }
+            }catch (error) {
+                node.log(error);
+                return false;
+            }
+            return false;
+        }
 
-        //init i2c module
+        //set all pins on hardware
+        function setPins(pins, state, cmd, signalName="", oldState=""){
+            for(let i=0;i<pins.length;i++){
+                const pinConf = pinMap[pins[i]];
+                var futurePins = 0b00000000;
+
+                if(state === "HIGH"){
+                    if(pinConf[0] == 0x12){
+                        futurePins = currentPinsA | pinConf[1];
+                        currentPinsA = futurePins;
+                    }else if(pinConf[0] == 0x13){
+                        futurePins = currentPinsB | pinConf[1];
+                        currentPinsB = futurePins;
+                    }
+                }else if(state === "LOW"){
+                    if(pinConf[0] == 0x12){
+                        futurePins = currentPinsA & ~pinConf[1];
+                        currentPinsA = futurePins;
+                    }else if(pinConf[0] == 0x13){
+                        futurePins = currentPinsB & ~pinConf[1];
+                        currentPinsB = futurePins;
+                    }
+                }
+                
+                var buffer = new Buffer.from([pinConf[0], futurePins]);
+                node.port.i2cWrite(busAddress, 2, buffer, function(err) {if (err) {
+                    node.status({fill:"red",shape:"dot",text:"i2c error"});
+                    node.send([null,null,{"payload": {"timestamp": Date.now(), "info": "i2c error"+err, "modulname": config.modname,"modulnumber": config.modnum}}]);
+                }});
+            }
+            //deactivate cmd
+            if(cmd){
+                node.send([null, {"payload": {"timestamp": Date.now(), "signal": signalName, "state": oldState, "info": "", "modulname": config.modname,"modulnumber": config.modnum}}, null]);
+                activeCMD = false;
+            }
+        }
+
+        //check for i2c module
+        if (!i2c) {
+            node.log("Couldn't load i2c-bus, try npm i i2c-bus");
+            node.status({fill:"gray",shape:"dot",text:"unsupported"});
+            if (i2c_error){
+                console.error(i2c_error);
+            }
+            node.send([null,null,{"payload": {"timestamp": Date.now(), "info": "Couldn't load i2c-bus, try npm i i2c-bus", "modulname": config.modname,"modulnumber": config.modnum}}]);
+            return false;
+        }
+
+        /*** INIT I2C MODULE ***/
         try{
-            node.port = i2c.open(1, err => {if (err) throw err;});
-            this.log(node.port);
-        } catch(error){
-            node.status({fill:"red",shape:"dot",text:"not connected to bus port"});
+            node.port = i2c.open(parseInt(config.busnum), err => {if (err) throw err;});
+            var buffer = new Buffer.from([0x00, 0x00]);
+            node.port.i2cWrite(busAddress, 2, buffer, function(err) {if (err) {node.error(err);}});
+            buffer = new Buffer.from([0x01, 0x00]);
+            node.port.i2cWrite(busAddress, 2, buffer, function(err) {if (err) {node.error(err);}});
+            buffer = new Buffer.from([0x12, 0x00]);
+            node.port.i2cWrite(busAddress, 2, buffer, function(err) {if (err) {node.error(err);}});
+            buffer = new Buffer.from([0x13, 0x00]);
+            node.port.i2cWrite(busAddress, 2, buffer, function(err) {if (err) {node.error(err);}});
+        } catch(err){
+            node.status({fill:"red",shape:"dot",text:"not connected to bus number"});
+            node.send([null,null,{"payload": {"timestamp": Date.now(), "info": "not connected to bus number", "modulname": config.modname,"modulnumber": config.modnum}}]);
+        } finally {
+            node.status({fill:"green",shape:"dot",text:"connected"});
         }
 
         //**** input handler ****//
         node.on('input', function(msg) {
             if(msg.payload === "pinstates"){
-                //TODO - I2C read
+                var pinstates0x12;
+                var pinstates0x13;
+                var output = [];
+
+                try{
+                    node.port.readWord(busAddress, 0x12, (err, rawData) => {
+                        if (err) throw err;
+
+                        pinstates0x12 = rawData;
+                        node.port.readWord(busAddress, 0x13, (err, rawData) => {
+                            if (err) throw err;
+                            pinstates0x13 = rawData; 
+                        
+                            //go through all signals
+                            for(let i=0;i<config.pinConfig.length; i++){
+                                const pinConf = pinMap[config.pinConfig[i][1]];
+                                var currentState = 0b00000000;
+
+                                if(pinConf[0] == 0x12){
+                                    currentState = pinstates0x12 & pinConf[1];
+                                }else if(pinConf[0] == 0x13){
+                                    currentState = pinstates0x13 & pinConf[1];
+                                }
+                                if(currentState != 0b00000000 && config.pinConfig[i][4] == "normal"){
+                                    output.push({"signalname": config.pinConfig[i][0], "state": "HIGH"});
+                                }else if(config.pinConfig[i][4] == "normal"){
+                                    output.push({"signalname": config.pinConfig[i][0], "state": "LOW"});
+                                }else if(config.pinConfig[i][4] == "inverted" && currentState != 0b00000000){
+                                    output.push({"signalname": config.pinConfig[i][0], "state": "LOW"});
+                                }else{
+                                    output.push({"signalname": config.pinConfig[i][0], "state": "HIGH"});
+                                }
+                            }
+                            msg.payload = output;
+                            this.send([msg,null,null]);
+                        });
+                    });
+                }catch(err){
+                    node.error(err);
+                    node.status({fill:"red",shape:"dot",text:"address not found. False bus number?"});
+                }
             }else if(msg.payload === "serialnumber"){
                 if(config.serialnum === ""){
-                    msg.payload = "command: serialnumber, but serialnumber is not specified";
+                    msg.payload = {"timestamp": Date.now(), "info": "command: serialnumber, but serialnumber is not specified", "modulname": config.modname,"modulnumber": config.modnum};
                     this.send([null,null,msg]);
                 }else{
-                    msg.payload = {"serialnumber": config.serialnum,"modulname": config.modname,"modulnumber": config.modnum};
+                    msg.payload = {"timestamp": Date.now(), "serialnumber": config.serialnum,"modulname": config.modname,"modulnumber": config.modnum};
                     this.send([null,msg,null]);
                 }
             }else if(validateMSG(msg.payload)){
-                const obj = JSON.parse(msg.payload);
+                const signalName = msg.payload["signalname"];
+                var oldState = msg.payload["state"];
+                var state = oldState;
+                var pins = [];
+                var type = "";
+                var duration = 0;
+                var inverted = false;
+
+                //check for type and load all pins
+                for(let i=0;i<config.pinConfig.length; i++){
+                    if(config.pinConfig[i][0] == signalName){
+                        type = config.pinConfig[i][2];
+                        duration = config.pinConfig[i][3];
+                        pins.push(config.pinConfig[i][1]);
+                        if(config.pinConfig[i][4] == "inverted" && !inverted){
+                            state = (oldState=="HIGH") ? "LOW":"HIGH";
+                            inverted = true;
+                        }
+                    }
+                }
                 
+                if(type === "info"){
+                    setPins(pins, state, false);
+                    msg.payload = {"timestamp": Date.now(), "signal": signalName, "state": oldState, "info": "", "modulname": config.modname,"modulnumber": config.modnum};
+                    node.send([null, msg, null]);
+                }else if(type === "cmd"){
+                    if(activeCMD){
+                        msg.payload = {"timestamp": Date.now(), "signal": signalName, "state": oldState, "info": "cmd currently running", "modulname": config.modname,"modulnumber": config.modnum};
+                        this.send([null,msg,null]);
+                    }else{
+                        msg.payload = {"timestamp": Date.now(), "signal": signalName, "state": oldState, "info": "", "modulname": config.modname,"modulnumber": config.modnum};
+                        node.send([null, msg, null]);
+                        setPins(pins, state, false);
+                        activeCMD = true;
+                        state = (state=="HIGH") ? "LOW":"HIGH";
+                        oldState = (oldState=="HIGH") ? "LOW":"HIGH";
+                        setTimeout(function(){setPins(pins, state, true, oldState, signalName)}, duration*1000);
+                    }
+                }
+            }else{
+                /* Diagnose msg */
+                const payload = msg.payload
+                msg.payload = {"timestamp": Date.now(), "signal": "", "state": "", "info": "Could not resole input: "+payload.toString(), "modulname": config.modname,"modulnumber": config.modnum};
+                node.send([null, msg, null]);
             }
         });
 
